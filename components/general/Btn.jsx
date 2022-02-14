cbb23b5764c3cee62dbe426d1cdb5712561045d8
arronrees <64908445+arronrees@users.noreply.github.com>
2022-02-14 15:08:05 +0000

    Changed styles file structure

--- components/general/Btn.jsx
+++ components/general/Btn.jsx
@@ -5,7 +5,7 @@ import Arrow from '../vectors/Arrow';
 export default function Btn({ href, text, className }) {
   return (
     <Link href={href}>
-      <a className={`btn ${className}`}>
+      <a className={`btn ${className ? className : ''}`}>
         <span>{text}</span>
         <div className='arrow'>
           <Arrow />
