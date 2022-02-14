2387da5189b859596ffd64c50c839fa9811a3d91
arronrees <64908445+arronrees@users.noreply.github.com>
2022-02-14 13:51:48 +0000

    Added loader

--- pages/_app.js
+++ pages/_app.js
@@ -4,10 +4,15 @@ import '../styles/style.css';
 
 import Header from '../components/layout/Header';
 import Cursor from '../components/general/Cursor';
+import Loader from '../components/general/Loader';
+import { useState } from 'react';
 
 function MyApp({ Component, pageProps }) {
+  const [firstLoad, setFirstLoad] = useState(true);
+
   return (
     <>
+      {/* <Loader firstLoad={firstLoad} setFirstLoad={setFirstLoad} /> */}
       <Header />
       <Cursor />
       <Component {...pageProps} />
