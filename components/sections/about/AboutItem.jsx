e9ce72c7a37340c146d7160f80614af8f59b4a79
arronrees <64908445+arronrees@users.noreply.github.com>
2022-02-15 09:43:42 +0000

    Changed about img animation

--- components/sections/about/AboutItem.jsx
+++ components/sections/about/AboutItem.jsx
@@ -8,61 +8,71 @@ export default function AboutItem({ item }) {
   const imageRef = useRef(null);
 
   useEffect(() => {
-    gsap.fromTo(
-      imageRef.current.querySelector('figure'),
-      { scaleX: 0 },
-      {
-        scaleX: 1,
-        duration: 0.8,
+    const introTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: imageRef.current,
+        start: 'top 70%',
+      },
+      defaults: {
         ease: 'power2.inOut',
-        scrollTrigger: {
-          trigger: imageRef.current,
-          start: 'top 70%',
+        duration: 1,
+      },
+    });
+
+    const slideTl = gsap.timeline({
+      scrollTrigger: {
+        scrub: 1,
+        trigger: imageRef.current,
+        start: 'top 70%',
+      },
+      defaults: {
+        ease: 'power2.inOut',
+        duration: 1,
+      },
+    });
+
+    introTl
+      .fromTo(
+        imageRef.current.querySelector('figure'),
+        { scaleX: 0 },
+        {
+          scaleX: 1,
+          ease: 'power2.inOut',
+        }
+      )
+      .fromTo(
+        imageRef.current.querySelector('.img__cover'),
+        { scaleX: 1 },
+        {
+          scaleX: 0,
+          ease: 'power2.inOut',
         },
-      }
-    );
-    gsap.fromTo(
-      imageRef.current.querySelector('.img__cover'),
-      { scaleX: 1 },
+        '-=0.6'
+      )
+      .fromTo(
+        imageRef.current.querySelector('img'),
+        { scale: 1.2 },
+        {
+          scale: 1,
+          ease: 'power2.inOut',
+        },
+        '-=0.8'
+      );
+
+    slideTl.fromTo(
+      imageRef.current,
+      { y: 0 },
       {
-        scaleX: 0,
-        delay: 0.4,
+        y: -150,
         duration: 1,
         ease: 'power2.inOut',
         scrollTrigger: {
+          scrub: 1,
           trigger: imageRef.current,
           start: 'top 70%',
         },
       }
     );
-    gsap.fromTo(
-      imageRef.current.querySelector('img'),
-      { scale: 1.2 },
-      {
-        scale: 1,
-        delay: 0.45,
-        duration: 1,
-        ease: 'power2.inOut',
-        scrollTrigger: {
-          trigger: imageRef.current,
-          start: 'top 70%',
-        },
-      }
-    );
-    // gsap.fromTo(
-    //   imageRef.current,
-    //   { y: 0 },
-    //   {
-    //     y: -150,
-    //     duration: 1,
-    //     ease: 'power2.inOut',
-    //     scrollTrigger: {
-    //       scrub: 1,
-    //       trigger: imageRef.current,
-    //       start: 'top 70%',
-    //     },
-    //   }
-    // );
   }, []);
 
   return (
