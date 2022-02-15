ca0dfe9980f072fac47d5db22bc1ee349502e37b
arronrees <64908445+arronrees@users.noreply.github.com>
2022-02-15 11:47:12 +0000

    Changed insights styles

--- components/sections/insights/InsightsInnerSection.jsx
+++ components/sections/insights/InsightsInnerSection.jsx
@@ -7,107 +7,84 @@ export default function InsightsInnerSection() {
         <div className='date'>02 / 02 / 2022</div>
         <h1 className='title'>News title will go in this space here</h1>
       </div>
+      <div className='insights__inner__section__img'>
+        <figure>
+          <Image src='/img.jpg' layout='fill' alt='#' />
+        </figure>
+      </div>
       <div className='insights__inner__section__content'>
-        <div className='images'>
-          <div className='img'>
-            <figure>
-              <Image src='/img.jpg' layout='fill' alt='#' />
-            </figure>
-          </div>
-          <div className='img'>
-            <figure>
-              <Image src='/img.jpg' layout='fill' alt='#' />
-            </figure>
-          </div>
-        </div>
-        <div className='content'>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. In dicta
-            rem reiciendis voluptates nostrum pariatur maiores est ratione,
-            perspiciatis quidem qui suscipit quae laborum porro accusantium?
-            Porro tempora veniam velit, officiis delectus consequatur recusandae
-            iste, ducimus vitae quasi distinctio perferendis nobis. Odit
-            consequatur, sapiente accusamus delectus mollitia obcaecati nisi
-            reprehenderit!
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. In dicta
-            rem reiciendis voluptates nostrum pariatur maiores est ratione,
-            perspiciatis quidem qui suscipit quae laborum porro accusantium?
-            Porro tempora veniam velit, officiis delectus consequatur recusandae
-            iste, ducimus vitae quasi distinctio perferendis nobis. Odit
-            consequatur, sapiente accusamus delectus mollitia obcaecati nisi
-            reprehenderit!
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. In dicta
-            rem reiciendis voluptates nostrum pariatur maiores est ratione,
-            perspiciatis quidem qui suscipit quae laborum porro accusantium?
-            Porro tempora veniam velit, officiis delectus consequatur recusandae
-            iste, ducimus vitae quasi distinctio perferendis nobis. Odit
-            consequatur, sapiente accusamus delectus mollitia obcaecati nisi
-            reprehenderit!
-          </p>
-          <p className='quote'>
-            <span>&quot;</span>
-            Images are very important for your website
-            <span>&quot;</span>
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. In dicta
-            rem reiciendis voluptates nostrum pariatur maiores est ratione,
-            perspiciatis quidem qui suscipit quae laborum porro accusantium?
-            Porro tempora veniam velit, officiis delectus consequatur recusandae
-            iste, ducimus vitae quasi distinctio perferendis nobis. Odit
-            consequatur, sapiente accusamus delectus mollitia obcaecati nisi
-            reprehenderit!
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. In dicta
-            rem reiciendis voluptates nostrum pariatur maiores est ratione,
-            perspiciatis quidem qui suscipit quae laborum porro accusantium?
-            Porro tempora veniam velit, officiis delectus consequatur recusandae
-            iste, ducimus vitae quasi distinctio perferendis nobis. Odit
-            consequatur, sapiente accusamus delectus mollitia obcaecati nisi
-            reprehenderit!
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. In dicta
-            rem reiciendis voluptates nostrum pariatur maiores est ratione,
-            perspiciatis quidem qui suscipit quae laborum porro accusantium?
-            Porro tempora veniam velit, officiis delectus consequatur recusandae
-            iste, ducimus vitae quasi distinctio perferendis nobis. Odit
-            consequatur, sapiente accusamus delectus mollitia obcaecati nisi
-            reprehenderit!
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. In dicta
-            rem reiciendis voluptates nostrum pariatur maiores est ratione,
-            perspiciatis quidem qui suscipit quae laborum porro accusantium?
-            Porro tempora veniam velit, officiis delectus consequatur recusandae
-            iste, ducimus vitae quasi distinctio perferendis nobis. Odit
-            consequatur, sapiente accusamus delectus mollitia obcaecati nisi
-            reprehenderit!
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. In dicta
-            rem reiciendis voluptates nostrum pariatur maiores est ratione,
-            perspiciatis quidem qui suscipit quae laborum porro accusantium?
-            Porro tempora veniam velit, officiis delectus consequatur recusandae
-            iste, ducimus vitae quasi distinctio perferendis nobis. Odit
-            consequatur, sapiente accusamus delectus mollitia obcaecati nisi
-            reprehenderit!
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. In dicta
-            rem reiciendis voluptates nostrum pariatur maiores est ratione,
-            perspiciatis quidem qui suscipit quae laborum porro accusantium?
-            Porro tempora veniam velit, officiis delectus consequatur recusandae
-            iste, ducimus vitae quasi distinctio perferendis nobis. Odit
-            consequatur, sapiente accusamus delectus mollitia obcaecati nisi
-            reprehenderit!
-          </p>
-        </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. In dicta rem
+          reiciendis voluptates nostrum pariatur maiores est ratione,
+          perspiciatis quidem qui suscipit quae laborum porro accusantium? Porro
+          tempora veniam velit, officiis delectus consequatur recusandae iste,
+          ducimus vitae quasi distinctio perferendis nobis. Odit consequatur,
+          sapiente accusamus delectus mollitia obcaecati nisi reprehenderit!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. In dicta rem
+          reiciendis voluptates nostrum pariatur maiores est ratione,
+          perspiciatis quidem qui suscipit quae laborum porro accusantium? Porro
+          tempora veniam velit, officiis delectus consequatur recusandae iste,
+          ducimus vitae quasi distinctio perferendis nobis. Odit consequatur,
+          sapiente accusamus delectus mollitia obcaecati nisi reprehenderit!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. In dicta rem
+          reiciendis voluptates nostrum pariatur maiores est ratione,
+          perspiciatis quidem qui suscipit quae laborum porro accusantium? Porro
+          tempora veniam velit, officiis delectus consequatur recusandae iste,
+          ducimus vitae quasi distinctio perferendis nobis. Odit consequatur,
+          sapiente accusamus delectus mollitia obcaecati nisi reprehenderit!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. In dicta rem
+          reiciendis voluptates nostrum pariatur maiores est ratione,
+          perspiciatis quidem qui suscipit quae laborum porro accusantium? Porro
+          tempora veniam velit, officiis delectus consequatur recusandae iste,
+          ducimus vitae quasi distinctio perferendis nobis. Odit consequatur,
+          sapiente accusamus delectus mollitia obcaecati nisi reprehenderit!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. In dicta rem
+          reiciendis voluptates nostrum pariatur maiores est ratione,
+          perspiciatis quidem qui suscipit quae laborum porro accusantium? Porro
+          tempora veniam velit, officiis delectus consequatur recusandae iste,
+          ducimus vitae quasi distinctio perferendis nobis. Odit consequatur,
+          sapiente accusamus delectus mollitia obcaecati nisi reprehenderit!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. In dicta rem
+          reiciendis voluptates nostrum pariatur maiores est ratione,
+          perspiciatis quidem qui suscipit quae laborum porro accusantium? Porro
+          tempora veniam velit, officiis delectus consequatur recusandae iste,
+          ducimus vitae quasi distinctio perferendis nobis. Odit consequatur,
+          sapiente accusamus delectus mollitia obcaecati nisi reprehenderit!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. In dicta rem
+          reiciendis voluptates nostrum pariatur maiores est ratione,
+          perspiciatis quidem qui suscipit quae laborum porro accusantium? Porro
+          tempora veniam velit, officiis delectus consequatur recusandae iste,
+          ducimus vitae quasi distinctio perferendis nobis. Odit consequatur,
+          sapiente accusamus delectus mollitia obcaecati nisi reprehenderit!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. In dicta rem
+          reiciendis voluptates nostrum pariatur maiores est ratione,
+          perspiciatis quidem qui suscipit quae laborum porro accusantium? Porro
+          tempora veniam velit, officiis delectus consequatur recusandae iste,
+          ducimus vitae quasi distinctio perferendis nobis. Odit consequatur,
+          sapiente accusamus delectus mollitia obcaecati nisi reprehenderit!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. In dicta rem
+          reiciendis voluptates nostrum pariatur maiores est ratione,
+          perspiciatis quidem qui suscipit quae laborum porro accusantium? Porro
+          tempora veniam velit, officiis delectus consequatur recusandae iste,
+          ducimus vitae quasi distinctio perferendis nobis. Odit consequatur,
+          sapiente accusamus delectus mollitia obcaecati nisi reprehenderit!
+        </p>
       </div>
     </section>
   );
