c3e7c7e91c4654a969cc7f1256347563dbcb3124
arronrees <64908445+arronrees@users.noreply.github.com>
2022-04-28 14:30:52 +0100

    Updated home hero block

--- components/general/LargeHero.jsx
+++ components/general/LargeHero.jsx
@@ -1,13 +1,40 @@
+import BtnNoLink from './BtnNoLink';
+import { useState, useRef, useEffect } from 'react';
+
 export default function LargeHero() {
+  const videoRef = useRef(null);
+
+  const [videoPlaying, setVideoPlaying] = useState(false);
+
   return (
     <section className='large__hero'>
-      <div className='large__hero__img'></div>
-      <div className='large__hero__text'>
-        <h1>Headline goes here</h1>
-        <h2 className='testing'>
+      <div className='video'>
+        <video ref={videoRef}>
+          <source src='/showreel.mp4' type='video/mp4' />
+        </video>
+      </div>
+      <div className={`large__hero__text ${videoPlaying ? 'playing' : ''}`}>
+        <h1 className='testing'>
           <span>Building brands </span>
           <span>for the future</span>
-        </h2>
+        </h1>
+        <div
+          className='view'
+          onClick={() => {
+            setVideoPlaying(!videoPlaying);
+
+            if (videoPlaying) {
+              videoRef.current.pause();
+            } else {
+              videoRef.current.play();
+            }
+          }}
+        >
+          <BtnNoLink
+            href='/'
+            text={videoPlaying ? 'Pause showreel' : 'Watch showreel'}
+          />
+        </div>
       </div>
     </section>
   );
