d3ab1e5143dfee5ba5e7d06b43229113e1b150d9
arronrees <64908445+arronrees@users.noreply.github.com>
2022-04-29 10:17:09 +0100

    Updated portfolio page

---
+++ components/sections/sliders/PortfolioSplide.jsx
@@ -0,0 +1,75 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import Arrow from '../../vectors/Arrow';
+
+export default function PortfolioSplide({ portfolio }) {
+  return (
+    <Splide
+      options={{
+        arrows: false,
+        perPage: 3,
+        drag: 'free',
+        gap: '4rem',
+        focus: 'center',
+        trimSpace: false,
+        breakpoints: {
+          1440: {
+            perPage: 2,
+          },
+          800: {
+            perPage: 1,
+            padding: {
+              right: '8rem',
+              left: '4rem',
+            },
+            gap: '2rem',
+          },
+          500: {
+            perPage: 1,
+            padding: {
+              right: '4rem',
+              left: '2rem',
+            },
+            gap: '2rem',
+          },
+        },
+      }}
+      className='services__splide'
+    >
+      {portfolio.map((item, i) => (
+        <SplideSlide key={i}>
+          <div className='img'>
+            <figure>
+              <Image
+                src={`http://localhost:1337${item.attributes.mainImage.data.attributes.url}`}
+                layout='fill'
+                alt={item.attributes.title}
+                priority='true'
+                className='services__slider__img'
+              />
+            </figure>
+          </div>
+          <div className='content'>
+            <Link href={`/services/${item.attributes.link}`}>
+              <a>
+                <h3>
+                  <span className='number'>{item.attributes.number}</span>
+                  <span className='text'>
+                    {item.attributes.title}
+                    <div className='underline'></div>
+                  </span>
+                  <span className='arrow'>
+                    <Arrow />
+                  </span>
+                </h3>
+              </a>
+            </Link>
+            <p>{item.attributes.summary}</p>
+          </div>
+        </SplideSlide>
+      ))}
+    </Splide>
+  );
+}
--- pages/work/premier-traffic.jsx
+++ pages/work/premier-traffic.jsx
@@ -5,88 +5,168 @@ import Seo from '../../components/layout/Seo';
 import Layout from '../../components/layout/Layout';
 import Footer from '../../components/layout/Footer';
 import Image from 'next/image';
+import Link from 'next/link';
+import Btn from '../../components/general/Btn';
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import Arrow from '../../components/vectors/Arrow';
+import { Fragment } from 'react';
+import PortfolioSplide from '../../components/sections/sliders/PortfolioSplide';
 
-export default function PremierTraffic() {
+export default function PremierTraffic({ portfolio, portfolios }) {
   return (
     <Layout>
       <Seo title={'Premier Traffic'} />
-      <main className='work'>
-        <InnerPortfolioHeader
-          imageSrc='/img.jpg'
-          imageAlt='premier'
-          title='Premier Traffic'
-          bgColour='#1d1d1b'
-        />
-        <section className='premier__details portfolio__details'>
-          <div className='grid'>
-            <div className='brochure'>
-              <div className='img'>
-                <figure>
-                  <Image
-                    src='/premier/brochures.png'
-                    width='2267'
-                    height='1970'
-                    alt='#'
-                    layout='responsive'
-                  />
-                </figure>
-              </div>
+      <main className='work__main work__main--premier'>
+        <section className='work__heading'>
+          <div className='black'></div>
+          <div className='title'>
+            <ul>
+              <li>Web Design &amp; Development</li>
+              <li>-</li>
+              <li>Print Design</li>
+            </ul>
+            <div>
+              <h1>{portfolio.attributes.title}</h1>
+              <Btn href='/project-planner' text='Plan your project' />
             </div>
-            <div className='posters'>
-              <div className='img'>
-                <figure>
-                  <Image
-                    src='/premier/posters.png'
-                    width='2445'
-                    height='1456'
-                    alt='#'
-                    layout='responsive'
-                  />
-                </figure>
-              </div>
+          </div>
+          <div className='main__img'>
+            <figure>
+              <Image
+                src={`http://localhost:1337${portfolio.attributes.mainImage.data.attributes.url}`}
+                layout='fill'
+                alt=''
+                priority='true'
+              />
+            </figure>
+          </div>
+        </section>
+        <section className='intro__details'>
+          <h2>{portfolio.attributes.summary}</h2>
+        </section>
+        <section className='showcase__img'>
+          <figure>
+            <Image src={'/phones.jpg'} layout='fill' alt='' />
+          </figure>
+        </section>
+        <section className='project__details'>
+          <div>
+            <div className='left'>
+              <ul>
+                <li>
+                  <span>What we did</span>
+                  <span>- Website Design &amp; Development</span>
+                  <span>- Print Design</span>
+                </li>
+                <li>
+                  <span>Website</span>
+                  <span>
+                    <a
+                      href='https://premier-traffic.co.uk'
+                      rel='noreferrer nofollower'
+                      target='_blank'
+                    >
+                      - premier-traffic.co.uk
+                    </a>
+                  </span>
+                </li>
+              </ul>
             </div>
-            <div className='cards'>
-              <div className='img'>
-                <figure>
-                  <Image
-                    src='/premier/cards.png'
-                    width='2525'
-                    height='1298'
-                    alt='#'
-                    layout='responsive'
-                  />
-                </figure>
-              </div>
-            </div>
-            <div className='logo'>
-              <figure>
-                <Image src='/premier/website.jpg' layout='fill' alt='#' />
-              </figure>
-            </div>
-            <div className='site__layout'>
-              <div className='phone'>
-                <figure>
-                  <Image src='/premier/phone.png' layout='fill' alt='#' />
-                </figure>
-              </div>
-              <div className='full'>
-                <figure>
-                  <Image
-                    src='/premier/website.jpg'
-                    width='1920'
-                    height='4736'
-                    alt='#'
-                    layout='responsive'
-                  />
-                </figure>
-              </div>
+            <div className='right'>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
+                dicta maiores libero suscipit dolores maxime aspernatur aut
+                minus debitis distinctio?
+              </p>
             </div>
           </div>
         </section>
-        <InnerPortfolioPerson bgColour='#f9e01d' />
-        <InnerPortfolioNext />
+        <section className='work__img__slider'>
+          <Splide
+            options={{
+              rewind: false,
+              arrows: false,
+              pagination: false,
+              perPage: 1,
+              perMove: 1,
+              padding: '10rem',
+              focus: 'center',
+              gap: '6rem',
+              updateOnMove: true,
+            }}
+            className='work__img__splide'
+          >
+            {portfolio.attributes.sliderImages.data.map((img) => (
+              <Fragment key={img.id}>
+                <SplideSlide>
+                  <div className='img'>
+                    <figure>
+                      <Image
+                        src={`http:localhost:1337${img.attributes.url}`}
+                        alt='#'
+                        layout='fill'
+                      />
+                    </figure>
+                  </div>
+                </SplideSlide>
+              </Fragment>
+            ))}
+            <div className='splide__progress'>
+              <div className='splide__progress__bar' />
+            </div>
+          </Splide>
+        </section>
+        <section className='work__image__grid'>
+          <div className='text__header'>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Consectetur veniam autem quidem quam voluptates ratione minima,
+              cumque deleniti odio expedita quibusdam exercitationem non
+              voluptas.
+            </p>
+          </div>
+          <div className='grid'>
+            <div className='img'>
+              <figure>
+                <Image src='/premier/phone.png' alt='#' layout='fill' />
+              </figure>
+            </div>
+            <div className='img'>
+              <figure>
+                <Image src='/premier/phone.png' alt='#' layout='fill' />
+              </figure>
+            </div>
+            <div className='img'>
+              <figure>
+                <Image src='/premier/website.jpg' alt='#' layout='fill' />
+              </figure>
+            </div>
+          </div>
+        </section>
+        <InnerPortfolioPerson bgColour='rgb(19, 19, 19)' colour='#fff' />
+        <section className='more__work__slider'>
+          <PortfolioSplide portfolio={portfolios} />
+        </section>
       </main>
       <Footer />
     </Layout>
   );
+}
+
+export async function getStaticProps({ params }) {
+  const portfolioRes = await fetch(
+    'http://localhost:1337/api/portfolios?populate=*'
+  );
+  const portfolioData = await portfolioRes.json();
+
+  const portfolio = portfolioData.data.filter(
+    (item) => item.attributes.title === 'Premier Traffic'
+  );
+
+  return {
+    props: {
+      portfolios: portfolioData.data,
+      portfolio: portfolio[0],
+    },
+  };
 }
